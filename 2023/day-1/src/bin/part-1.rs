4400bff7aff8ac4b0a38b8bfdd04fa97e40c199d
Jan Sipr <sipr.jan@gmail.com>
2023-12-01 16:42:11 +0100

    More lints

--- 2023/day-1/src/bin/part-1.rs
+++ 2023/day-1/src/bin/part-1.rs
@@ -7,13 +7,13 @@ fn main() {
         .map(|l| {
             let first = l
                 .chars()
-                .find(|c| c.is_digit(10))
+                .find(|c| c.is_ascii_digit())
                 .and_then(|c| c.to_digit(10))
                 .unwrap();
             let last = l
                 .chars()
                 .rev()
-                .find(|c| c.is_digit(10))
+                .find(|c| c.is_ascii_digit())
                 .and_then(|c| c.to_digit(10))
                 .unwrap();
             (first * 10) + last
