49f90240cf007de0a0c89b74df96b9784d2f4a24
Jan Sipr <sipr.jan@gmail.com>
2023-12-06 21:37:43 +0100

    Improving the solution with quadratic equation

--- 2023/day-6/src/bin/part-1.rs
+++ 2023/day-6/src/bin/part-1.rs
@@ -1,5 +1,27 @@
 use std::iter::zip;
 
+fn solve(t: i64, d:i64) -> i64 {
+    // The equation we are trying solve is `(t - x) x = y` after rewriting it
+    // we get `-x^2 + t = y` which is quadratic equation. If we move it using
+    // the record on the y-axis such as => `-x^2 + t - d = y` we get number
+    // over the record as positive numbers. So in the end we can approach it as
+    // problem were we try to find the roots of the quadratic equation.
+
+    let discriminant = (t*t) - 4 * d;
+    if discriminant <= 0 {
+        return 0;
+    }
+    // Coincidently the distance is between the two roots is
+    // `discriminant.sqrt()`, so let's use that.
+    let distance = (discriminant as f64).sqrt();
+    // Also the parity is bit of mess......
+    if t % 2 == 0 {
+        2 * (distance / 2.0).ceil() as i64 - 1
+    } else {
+        2 * ((distance + 1.0) / 2.0).ceil() as i64 - 2
+    }
+}
+
 fn parse_input(input: &str) -> Vec<(u32, u32)> {
     let mut lines = input.lines();
     let mut times = lines.next().unwrap().split_whitespace();
@@ -14,11 +36,8 @@ fn parse_input(input: &str) -> Vec<(u32, u32)> {
 }
 fn part_1(input: &str) -> String {
     let data = parse_input(input);
-    let kwa = data.iter().map(|(t, d)| {
-        (1..*t).map(|td| td * (t - td)).filter(|my_distance| my_distance > d).collect::<Vec<u32>>()
-    }).collect::<Vec<Vec<u32>>>();
 
-    kwa.iter().map(|times| times.len()).product::<usize>().to_string()
+    data.iter().map(|(t, d)|solve(*t as i64, *d as i64)).product::<i64>().to_string()
 }
 
 fn main() {
--- 2023/day-6/src/bin/part-2.rs
+++ 2023/day-6/src/bin/part-2.rs
@@ -1,17 +1,40 @@
 fn parse_input(input: &str) -> (u64, u64) {
     let mut lines = input.lines();
-    let times = lines.next().unwrap().split_once(":").unwrap().1;
+    let times = lines.next().unwrap().split_once(':').unwrap().1;
     let time = times.chars().filter(|c| !c.is_whitespace()).collect::<String>();
     let time = time.parse::<u64>().unwrap();
-    let distances = lines.next().unwrap().split_once(":").unwrap().1;
+    let distances = lines.next().unwrap().split_once(':').unwrap().1;
     let distance = distances.chars().filter(|c| !c.is_whitespace()).collect::<String>();
     let distance = distance.parse::<u64>().unwrap();
     (time, distance)
 }
 
+fn solve(t: i64, d:i64) -> i64 {
+    // The equation we are trying solve is `(t - x) x = y` after rewriting it
+    // we get `-x^2 + t = y` which is quadratic equation. If we move it using
+    // the record on the y-axis such as => `-x^2 + t - d = y` we get number
+    // over the record as positive numbers. So in the end we can approach it as
+    // problem were we try to find the roots of the quadratic equation.
+
+    let discriminant = (t*t) - 4 * d;
+    if discriminant <= 0 {
+        return 0;
+    }
+    // Coincidently the distance is between the two roots is
+    // `discriminant.sqrt()`, so let's use that.
+    let distance = (discriminant as f64).sqrt();
+    // Also the parity is bit of mess......
+    if t % 2 == 0 {
+        2 * (distance / 2.0).ceil() as i64 - 1
+    } else {
+        2 * ((distance + 1.0) / 2.0).ceil() as i64 - 2
+    }
+}
+
 fn part_2(input: &str) -> String {
     let (time, distance) = parse_input(input);
-    (1..time).map(|td| td * (time - td)).filter(|my_distance| my_distance > &distance).collect::<Vec<u64>>().len().to_string()
+
+    solve(time as i64, distance as i64).to_string()
 }
 
 fn main() {
