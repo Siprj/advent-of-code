3477f59b53af01949ea3e8a5e72ac2c31ea84dd0
Jan Sipr <sipr.jan@gmail.com>
2024-12-20 17:59:37 +0100

    Day 19

--- 2024/day-19/src/bin/part-1.rs
+++ 2024/day-19/src/bin/part-1.rs
@@ -1,136 +1,118 @@
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashSet},
-};
+use std::collections::{BinaryHeap, HashMap};
 
-fn parse(input: &str) -> Vec<(i32, i32)> {
-    input
-        .trim()
-        .lines()
-        .map(|l| {
-            let (x, y) = l.split_once(',').unwrap();
-            (y.parse().unwrap(), x.parse().unwrap())
-        })
-        .collect()
+fn parse(input: &str) -> (Vec<&str>, Vec<&str>) {
+    let (available, patterns) = input.trim().split_once("\n\n").unwrap();
+    let available = available.split(", ").collect();
+    let patterns = patterns.split_whitespace().collect();
+    (available, patterns)
 }
 
 #[derive(Debug)]
 struct Node {
-    x: i32,
-    y: i32,
-    count: usize,
+    next: HashMap<char, Box<Node>>,
 }
-impl PartialEq for Node {
-    fn eq(&self, other: &Self) -> bool {
-        self.count.eq(&other.count)
+
+impl Node {
+    fn new() -> Self {
+        Node {
+            next: HashMap::new(),
+        }
     }
 }
 
-impl Eq for Node {}
+fn make_machine(available: &[&str]) -> HashMap<char, Box<Node>> {
+    let mut machine: HashMap<char, Box<Node>> = HashMap::new();
+    for a in available.iter() {
+        let mut char_iter = a.chars();
+        let first_char: char = char_iter.next().unwrap();
+        let entry = machine.entry(first_char);
+        let mut node = entry.or_insert(Box::new(Node::new()));
+        for c in char_iter {
+            let entry = node.next.entry(c);
+            node = entry.or_insert(Box::new(Node::new()));
+        }
+        node.next.entry('#').or_insert(Box::new(Node::new()));
+    }
+    machine
+}
 
-impl PartialOrd for Node {
+#[derive(Debug)]
+struct Token<'a> {
+    index: usize,
+    mach: &'a Box<Node>,
+}
+
+impl<'a> PartialEq for Token<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.index.eq(&other.index)
+    }
+}
+
+impl<'a> Eq for Token<'a> {}
+
+impl<'a> PartialOrd for Token<'a> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for Node {
+impl<'a> Ord for Token<'a> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.count.cmp(&other.count)
+        self.index.cmp(&other.index)
     }
 }
 
-fn is_valid(map: &[Vec<bool>], (y, x): (i32, i32)) -> bool {
-    let width = map[0].len();
-    let height = map.len();
+fn part_1(input: &str) -> String {
+    let (available, patterns) = parse(input);
+    let machine = Box::new(Node {
+        next: make_machine(&available),
+    });
 
-    if y < 0 || y >= height as i32 || x < 0 || x >= width as i32 {
-        return false;
-    }
+    let mut count_designes: i32 = 0;
+    'outer: for pattern in patterns {
+        let pattern: Vec<char> = pattern.chars().collect();
+        //let mut node = &machine;
+        let mut max_heap = BinaryHeap::with_capacity(1000);
+        max_heap.push(Token {
+            index: 0,
+            mach: &machine,
+        });
+        let pattern_len = pattern.len();
+        let mut k = 0;
+        while let Some(t) = max_heap.pop() {
+            k += 1;
+            if k > 1000 {
+                continue 'outer;
+            }
 
-    !map[y as usize][x as usize]
-}
+            if t.index == pattern_len {
+                if t.mach.next.contains_key(&'#') {
+                    count_designes += 1;
+                    continue 'outer;
+                }
+            } else {
+                if let Some(next) = t.mach.next.get(&pattern[t.index]) {
+                    max_heap.push(Token {
+                        index: t.index + 1,
+                        mach: next,
+                    });
+                }
 
-fn get_nexts(map: &[Vec<bool>], cache: &mut HashSet<(i32, i32)>, node: &Node) -> Vec<Node> {
-    let mut ret = Vec::new();
-    let next_y = node.y + 1;
-    let next_x = node.x;
-    let next_node = Node {
-        x: next_x,
-        y: next_y,
-        count: node.count + 1,
-    };
-    if is_valid(map, (next_y, next_x)) && !cache.contains(&(next_y, next_x)) {
-        cache.insert((next_y, next_x));
-        ret.push(next_node)
-    }
-    let next_y = node.y - 1;
-    let next_x = node.x;
-    let next_node = Node {
-        x: next_x,
-        y: next_y,
-        count: node.count + 1,
-    };
-    if is_valid(map, (next_y, next_x)) && !cache.contains(&(next_y, next_x)) {
-        cache.insert((next_y, next_x));
-        ret.push(next_node)
-    }
-    let next_y = node.y;
-    let next_x = node.x + 1;
-    let next_node = Node {
-        x: next_x,
-        y: next_y,
-        count: node.count + 1,
-    };
-    if is_valid(map, (next_y, next_x)) && !cache.contains(&(next_y, next_x)) {
-        cache.insert((next_y, next_x));
-        ret.push(next_node)
-    }
-    let next_y = node.y;
-    let next_x = node.x - 1;
-    let next_node = Node {
-        x: next_x,
-        y: next_y,
-        count: node.count + 1,
-    };
-    if is_valid(map, (next_y, next_x)) && !cache.contains(&(next_y, next_x)) {
-        cache.insert((next_y, next_x));
-        ret.push(next_node)
-    }
-    ret
-}
-
-fn part_1(input: &str, width: i32, height: i32, iterations: usize) -> String {
-    let falling = parse(input);
-    let mut map: Vec<Vec<bool>> = (0..height)
-        .map(|_| (0..width).map(|_| false).collect())
-        .collect();
-    for f in falling.iter().take(iterations) {
-        map[f.0 as usize][f.1 as usize] = true;
-    }
-
-    let mut queue: BinaryHeap<Reverse<Node>> = BinaryHeap::new();
-    queue.push(Reverse(Node {
-        x: 0,
-        y: 0,
-        count: 0,
-    }));
-    let mut cache = HashSet::new();
-    while let Some(Reverse(node)) = queue.pop() {
-        if node.x == width - 1 && node.y == height - 1 {
-            return node.count.to_string();
-        }
-        for n in get_nexts(&map, &mut cache, &node) {
-            queue.push(Reverse(n));
+                if let Some(_end) = t.mach.next.get(&'#') {
+                    max_heap.push(Token {
+                        index: t.index,
+                        mach: &machine,
+                    });
+                }
+            }
         }
     }
-
-    panic!("not cool");
+    count_designes.to_string()
 }
 
 fn main() {
     let input = include_str!("input.txt");
-    let result = part_1(input, 71, 71, 1024);
+    let result = part_1(input);
     println!("Part 1: {}", result);
 }
 
@@ -140,31 +122,24 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let input: &str = "5,4
-4,2
-4,5
-3,0
-2,1
-6,3
-2,4
-1,5
-0,6
-3,3
-2,6
-5,1
-1,2
-5,5
-2,5
-6,5
-1,4
-0,4
-6,4
-1,1
-6,1
-1,0
-0,5
-1,6
-2,0";
-        assert_eq!(part_1(input, 7, 7, 12), "22");
+        let input: &str = "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+        assert_eq!(part_1(input), "6");
     }
+
+    //    #[test]
+    //    fn it_works2() {
+    //        let input: &str = "rug, uugbb, bubbbr, bbubw, wwb, wbw, gwbbuw, ubg, ruwug, bbwuww, wubwrw, ug, ugu, ggrbg, ruw, rbur, uuubbuu, wb, wurrbgr, rwruwwuu, rwg, rwbb, bwugugg, bur, ggwub, brgbr, guuu, wgg, bbw, ugrbrb, ggrw, wbgu, rwgbg, ggwu, wurwuw, gruub, gugw, bgu, wrr, rbrg, bww, uggwrr, rgg, uugb, bbrwuru, rw, urbu, ggggg, ru, wrrgwrrb, uurrgur, bgrw, ubuwr, gwwgug, bggb, uwub, gwu, wrwbgw, rrbb, uuwrggb, uuugw, bbbubg, bg, ggr, rgw, ugburuw, ggbbww, rrgbru, wurg, wugu, rbuu, wgrg, ubugu, uuwuur, ur, wgbbg, wubu, gbrrwgu, bwbu, wbgguwg, rrwbwru, buggggg, bwu, uw, grubg, bggbw, ugwwgwu, rbubgwg, wgwuu, rrr, uubwwgrr, bbwguw, uuru, wgww, grbwu, gbgww, uuu, gruwwg, urgw, ubu, ggbgb, buwur, bbgb, bub, wbru, gbug, wgrruub, wrwwru, wuwgg, uwg, uuwgw, rrruwb, rbr, grrrugwb, rbgb, wwbr, ggbugg, ggg, buu, uwr, guug, wg, wbwu, wgb, rbbb, rub, wru, bubr, rgruwrgr, gwr, urru, wuw, uuwg, bbr, ruwg, urwgu, bwbugbb, wgbgrwgg, grwb, rwgg, uugg, uww, w, urwgrgb, gw, ubrb, grbrrbrw, wgwgrr, rggurb, buuww, bgrrb, wbrg, ggburu, rgwgb, wrw, bw, wubbb, bbwww, ubbg, bbg, ruururru, rbg, wgug, gur, rrugw, gwurbg, gr, bwwgrr, gub, wrgwrw, gbrrb, rr, rgbu, bbrur, ubgbu, wuwrw, guub, guu, uwbww, gguuww, ubuw, uuggu, wgwg, grwrr, bwg, uwgrb, grwu, gwbw, rwu, buubrb, ggbubw, wrgw, wwu, uuubwrgb, urwgbwu, rwggwrg, uuuw, gbrwb, bbuu, urgbgb, www, guw, uuw, wbubg, brw, gubruu, uwrg, bbbggg, bwr, wgbg, wguw, rbbgruru, uubgggr, ugw, grgugr, uugrrbb, rwgwg, ubgu, burgwgbw, gwrubbu, bwuug, bburur, burwu, wbb, ub, wr, ggubww, grw, ww, buw, urbru, gbgr, burwgur, ugr, gwwbgb, urr, wuwg, bbubrrgu, wuuwb, brrwg, g, gbgurrw, rgu, ubb, urggug, wub, gwubu, bgrr, wrbwwb, guuuuug, rrb, brugwu, wgrgwu, bru, wrbgugur, ubww, uur, uuub, wubwrb, uubggur, bbu, urb, rrggwuw, brgw, urbrrg, uwuur, bubw, bbwuu, gbu, rrg, rrgbw, gbr, wbu, ubrw, rww, ubw, wugr, rrur, bggu, uurrbwr, br, ggu, bgww, gwbbubgu, grwbw, uu, wguwg, urwubg, rruwg, wur, rgbwg, wbrgbg, ugrr, wbbggu, gww, bugb, wrbr, grurrg, gbrgwb, rur, gubbr, gbb, gggubwgu, gwbbbwru, rbbbu, rg, gwg, bgg, rgbg, uubbrwb, uwur, rwr, uwu, wbg, wwwbgurr, grb, gbg, rugg, wgub, bb, bgb, ubwbrww, gugbr, wwgwwwug, brb, rbb, uwgg, ubbbw, rwwr, urrw, ubuuub, wbub, ugbrr, ubrr, bgwr, gwb, wuuw, ggbuwb, wug, wgugr, urgrb, uwbbwr, gwgb, uug, gbur, wwr, gug, rgbubw, ruu, gbbw, grg, ggruwgb, urwggu, rrw, rgguuu, bgwg, uub, rbu, bu, grrr, r, ugb, bbrb, gwwbr, bwwur, wwg, grurrgb, wrguuu, rgggw, ugbwg, rwb, wubgw, grrww, uwb, bgw, bggrw, rgr, rbwg, buugbbb, bgr, gbgrgr, wbr, wgr, wbbr, gg, gru, urg, bguu, bbwbgur, bburgw, ubr, wrg, gu, rbw, rwbrur, bwgru, ubwrugb, uggubw, rgbr, bbgg, wrb, wwuug, bwbuw, rb, gwugbr, uuwrw, u, rburg, wguwgwu, bwrru, uguwgb, wuu, ugg, bgrg, wrrw, brg, rwwru, buuwg, ubuuwwr, ruwr, bbb, guwuwuug, rrbgg, ugwub, rrwwugb, bug, wgw, urw, brr, bwuwrw, bbug, rru, brbubb, bwb, ggbr, rgb, gwrwb, grbgw, rrwuruw, wruuwgrb, brruubu, bgwbu, grr, ubru, rbrr, wubwuub, grbr
+    //
+    //bwbbrrgrrbrggubuggwgguguburbbgbgrruggugbggggb";
+    //        assert_eq!(part_1(input), "0");
+    //    }
 }
--- 2024/day-19/src/bin/part-2.rs
+++ 2024/day-19/src/bin/part-2.rs
@@ -1,148 +1,117 @@
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashSet},
-};
+use std::collections::HashMap;
 
-fn parse(input: &str) -> Vec<(i32, i32)> {
-    input
-        .trim()
-        .lines()
-        .map(|l| {
-            let (x, y) = l.split_once(',').unwrap();
-            (y.parse().unwrap(), x.parse().unwrap())
-        })
-        .collect()
+fn parse(input: &str) -> (Vec<&str>, Vec<&str>) {
+    let (available, patterns) = input.trim().split_once("\n\n").unwrap();
+    let available = available.split(", ").collect();
+    let patterns = patterns.split_whitespace().collect();
+    (available, patterns)
 }
 
 #[derive(Debug)]
 struct Node {
-    x: i32,
-    y: i32,
-    count: usize,
-}
-impl PartialEq for Node {
-    fn eq(&self, other: &Self) -> bool {
-        self.count.eq(&other.count)
-    }
+    nodes: HashMap<char, Node>,
+    is_leaf: bool,
 }
 
-impl Eq for Node {}
-
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Node {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.count.cmp(&other.count)
-    }
-}
-
-fn is_valid(map: &[Vec<bool>], (y, x): (i32, i32)) -> bool {
-    let width = map[0].len();
-    let height = map.len();
-
-    if y < 0 || y >= height as i32 || x < 0 || x >= width as i32 {
-        return false;
-    }
-
-    !map[y as usize][x as usize]
-}
-
-fn get_nexts(map: &[Vec<bool>], cache: &mut HashSet<(i32, i32)>, node: &Node) -> Vec<Node> {
-    let mut ret = Vec::new();
-    let next_y = node.y + 1;
-    let next_x = node.x;
-    let next_node = Node {
-        x: next_x,
-        y: next_y,
-        count: node.count + 1,
-    };
-    if is_valid(map, (next_y, next_x)) && !cache.contains(&(next_y, next_x)) {
-        cache.insert((next_y, next_x));
-        ret.push(next_node)
-    }
-    let next_y = node.y - 1;
-    let next_x = node.x;
-    let next_node = Node {
-        x: next_x,
-        y: next_y,
-        count: node.count + 1,
-    };
-    if is_valid(map, (next_y, next_x)) && !cache.contains(&(next_y, next_x)) {
-        cache.insert((next_y, next_x));
-        ret.push(next_node)
-    }
-    let next_y = node.y;
-    let next_x = node.x + 1;
-    let next_node = Node {
-        x: next_x,
-        y: next_y,
-        count: node.count + 1,
-    };
-    if is_valid(map, (next_y, next_x)) && !cache.contains(&(next_y, next_x)) {
-        cache.insert((next_y, next_x));
-        ret.push(next_node)
-    }
-    let next_y = node.y;
-    let next_x = node.x - 1;
-    let next_node = Node {
-        x: next_x,
-        y: next_y,
-        count: node.count + 1,
-    };
-    if is_valid(map, (next_y, next_x)) && !cache.contains(&(next_y, next_x)) {
-        cache.insert((next_y, next_x));
-        ret.push(next_node)
-    }
-    ret
-}
-
-fn find_path(map: &[Vec<bool>]) -> Option<Node>{
-    let width = map[0].len() as i32;
-    let height = map.len() as i32;
-    let mut queue: BinaryHeap<Reverse<Node>> = BinaryHeap::with_capacity(5000);
-    queue.push(Reverse(Node {
-        x: 0,
-        y: 0,
-        count: 0,
-    }));
-    let mut cache = HashSet::with_capacity(5000);
-    while let Some(Reverse(node)) = queue.pop() {
-        if node.x == width - 1 && node.y == height - 1 {
-            return Some(node);
-        }
-        for n in get_nexts(map, &mut cache, &node) {
-            queue.push(Reverse(n));
+impl Node {
+    fn new() -> Self {
+        Self {
+            nodes: HashMap::new(),
+            is_leaf: false,
         }
     }
-    None
 }
 
-fn part_2(input: &str, width: i32, height: i32, mut iterations: usize) -> String {
-    let falling = parse(input);
-    let mut map: Vec<Vec<bool>> = (0..height)
-        .map(|_| (0..width).map(|_| false).collect())
-        .collect();
-    for f in falling.iter().take(iterations) {
-        map[f.0 as usize][f.1 as usize] = true;
+#[derive(Debug)]
+struct PrefixTree {
+    root: Node,
+}
+
+impl PrefixTree {
+    fn new() -> Self {
+        PrefixTree {
+            root: Node {
+                nodes: HashMap::new(),
+                is_leaf: false,
+            },
+        }
     }
 
-    map[falling[iterations].0 as usize][falling[iterations].1 as usize] = true;
-
-    while find_path(&map).is_some() {
-        iterations += 1;
-        map[falling[iterations].0 as usize][falling[iterations].1 as usize] = true;
+    fn build_from_list(available: &[&str]) -> PrefixTree {
+        let mut prefix_tree = PrefixTree::new();
+        for prefix in available.iter() {
+            prefix_tree.insert(prefix);
+        }
+        prefix_tree
     }
 
-    format!("{},{}", falling[iterations].1, falling[iterations].0).to_string()
+    fn insert(&mut self, str: &str) {
+        let mut node = &mut self.root;
+        for c in str.chars() {
+            let entry = node.nodes.entry(c);
+            entry.or_insert(Node::new());
+            node = node.nodes.get_mut(&c).unwrap();
+        }
+        node.is_leaf = true;
+    }
+
+    fn contains_prefix(&self, str: &str) -> bool {
+        let mut node = &self.root;
+        for c in str.chars() {
+            if let Some(next) = node.nodes.get(&c) {
+                node = next;
+            } else {
+                return false;
+            }
+        }
+        node.is_leaf
+    }
+
+    fn get_prefixes<'a>(&self, str: &'a str) -> Vec<&'a str> {
+        let mut ret: Vec<&str> = Vec::new();
+        for i in 1..=str.len() {
+            if self.contains_prefix(&str[0..i]) {
+                ret.push(&str[0..i]);
+            }
+        }
+        ret
+    }
+}
+
+fn count_posible<'a>(str: &'a str, index: usize, prefix_tree: &PrefixTree, cache: &mut HashMap<&'a str, usize>) -> usize {
+    if index >= str.len() {
+        return 1;
+    }
+
+    let mut sum = 0;
+    let prefixes: Vec<&str> = prefix_tree.get_prefixes(&str[index..]);
+    for prefix in prefixes.iter() {
+        let next_index = index + prefix.len();
+        if let Some(v) = cache.get(&str[next_index..]) {
+            sum += v;
+        } else {
+            let ret = count_posible(str, next_index, prefix_tree, cache);
+            sum += ret;
+        }
+    }
+    cache.insert(&str[index..], sum);
+    sum
+}
+
+fn part_2(input: &str) -> String {
+    let (available, patterns) = parse(input);
+    let prefix_tree = PrefixTree::build_from_list(&available);
+
+    let mut sum = 0;
+    for pattern in patterns.iter() {
+        sum += count_posible(pattern, 0, &prefix_tree, &mut HashMap::new());
+    }
+    sum.to_string()
 }
 
 fn main() {
     let input = include_str!("input.txt");
-    let result = part_2(input, 71, 71, 1024);
+    let result = part_2(input);
     println!("Part 2: {}", result);
 }
 
@@ -152,31 +121,25 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let input: &str = "5,4
-4,2
-4,5
-3,0
-2,1
-6,3
-2,4
-1,5
-0,6
-3,3
-2,6
-5,1
-1,2
-5,5
-2,5
-6,5
-1,4
-0,4
-6,4
-1,1
-6,1
-1,0
-0,5
-1,6
-2,0";
-        assert_eq!(part_2(input, 7, 7, 12), "6,1");
+        let input: &str = "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+
+        assert_eq!(part_2(input), "16");
+    }
+
+    #[test]
+    fn it_works2() {
+        let input: &str = "rug, uugbb, bubbbr, bbubw, wwb, wbw, gwbbuw, ubg, ruwug, bbwuww, wubwrw, ug, ugu, ggrbg, ruw, rbur, uuubbuu, wb, wurrbgr, rwruwwuu, rwg, rwbb, bwugugg, bur, ggwub, brgbr, guuu, wgg, bbw, ugrbrb, ggrw, wbgu, rwgbg, ggwu, wurwuw, gruub, gugw, bgu, wrr, rbrg, bww, uggwrr, rgg, uugb, bbrwuru, rw, urbu, ggggg, ru, wrrgwrrb, uurrgur, bgrw, ubuwr, gwwgug, bggb, uwub, gwu, wrwbgw, rrbb, uuwrggb, uuugw, bbbubg, bg, ggr, rgw, ugburuw, ggbbww, rrgbru, wurg, wugu, rbuu, wgrg, ubugu, uuwuur, ur, wgbbg, wubu, gbrrwgu, bwbu, wbgguwg, rrwbwru, buggggg, bwu, uw, grubg, bggbw, ugwwgwu, rbubgwg, wgwuu, rrr, uubwwgrr, bbwguw, uuru, wgww, grbwu, gbgww, uuu, gruwwg, urgw, ubu, ggbgb, buwur, bbgb, bub, wbru, gbug, wgrruub, wrwwru, wuwgg, uwg, uuwgw, rrruwb, rbr, grrrugwb, rbgb, wwbr, ggbugg, ggg, buu, uwr, guug, wg, wbwu, wgb, rbbb, rub, wru, bubr, rgruwrgr, gwr, urru, wuw, uuwg, bbr, ruwg, urwgu, bwbugbb, wgbgrwgg, grwb, rwgg, uugg, uww, w, urwgrgb, gw, ubrb, grbrrbrw, wgwgrr, rggurb, buuww, bgrrb, wbrg, ggburu, rgwgb, wrw, bw, wubbb, bbwww, ubbg, bbg, ruururru, rbg, wgug, gur, rrugw, gwurbg, gr, bwwgrr, gub, wrgwrw, gbrrb, rr, rgbu, bbrur, ubgbu, wuwrw, guub, guu, uwbww, gguuww, ubuw, uuggu, wgwg, grwrr, bwg, uwgrb, grwu, gwbw, rwu, buubrb, ggbubw, wrgw, wwu, uuubwrgb, urwgbwu, rwggwrg, uuuw, gbrwb, bbuu, urgbgb, www, guw, uuw, wbubg, brw, gubruu, uwrg, bbbggg, bwr, wgbg, wguw, rbbgruru, uubgggr, ugw, grgugr, uugrrbb, rwgwg, ubgu, burgwgbw, gwrubbu, bwuug, bburur, burwu, wbb, ub, wr, ggubww, grw, ww, buw, urbru, gbgr, burwgur, ugr, gwwbgb, urr, wuwg, bbubrrgu, wuuwb, brrwg, g, gbgurrw, rgu, ubb, urggug, wub, gwubu, bgrr, wrbwwb, guuuuug, rrb, brugwu, wgrgwu, bru, wrbgugur, ubww, uur, uuub, wubwrb, uubggur, bbu, urb, rrggwuw, brgw, urbrrg, uwuur, bubw, bbwuu, gbu, rrg, rrgbw, gbr, wbu, ubrw, rww, ubw, wugr, rrur, bggu, uurrbwr, br, ggu, bgww, gwbbubgu, grwbw, uu, wguwg, urwubg, rruwg, wur, rgbwg, wbrgbg, ugrr, wbbggu, gww, bugb, wrbr, grurrg, gbrgwb, rur, gubbr, gbb, gggubwgu, gwbbbwru, rbbbu, rg, gwg, bgg, rgbg, uubbrwb, uwur, rwr, uwu, wbg, wwwbgurr, grb, gbg, rugg, wgub, bb, bgb, ubwbrww, gugbr, wwgwwwug, brb, rbb, uwgg, ubbbw, rwwr, urrw, ubuuub, wbub, ugbrr, ubrr, bgwr, gwb, wuuw, ggbuwb, wug, wgugr, urgrb, uwbbwr, gwgb, uug, gbur, wwr, gug, rgbubw, ruu, gbbw, grg, ggruwgb, urwggu, rrw, rgguuu, bgwg, uub, rbu, bu, grrr, r, ugb, bbrb, gwwbr, bwwur, wwg, grurrgb, wrguuu, rgggw, ugbwg, rwb, wubgw, grrww, uwb, bgw, bggrw, rgr, rbwg, buugbbb, bgr, gbgrgr, wbr, wgr, wbbr, gg, gru, urg, bguu, bbwbgur, bburgw, ubr, wrg, gu, rbw, rwbrur, bwgru, ubwrugb, uggubw, rgbr, bbgg, wrb, wwuug, bwbuw, rb, gwugbr, uuwrw, u, rburg, wguwgwu, bwrru, uguwgb, wuu, ugg, bgrg, wrrw, brg, rwwru, buuwg, ubuuwwr, ruwr, bbb, guwuwuug, rrbgg, ugwub, rrwwugb, bug, wgw, urw, brr, bwuwrw, bbug, rru, brbubb, bwb, ggbr, rgb, gwrwb, grbgw, rrwuruw, wruuwgrb, brruubu, bgwbu, grr, ubru, rbrr, wubwuub, grbr
+
+    bwbbrrgrrbrggubuggwgguguburbbgbgrruggugbggggb";
+        assert_eq!(part_2(input), "0");
     }
 }
