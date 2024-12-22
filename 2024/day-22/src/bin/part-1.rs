407fb41df3efe80e833ed0dba31b3e57b1dae29e
Jan Sipr <sipr.jan@gmail.com>
2024-12-22 19:04:50 +0100

    Day 22

---
+++ 2024/day-22/src/bin/part-1.rs
@@ -0,0 +1,62 @@
+fn parse(input: &str) -> Vec<u64> {
+    input.trim()
+        .lines()
+        .map(|l| {
+            l.parse::<u64>().unwrap()
+        }).collect()
+}
+
+fn mix(secret: &mut u64, new_value: u64) {
+    *secret ^= new_value;
+}
+
+fn prune(secret: &mut u64) {
+    *secret %= 16777216;
+}
+
+fn random(mut secret: u64) -> u64 {
+    let mul = secret * 64;
+    mix(&mut secret, mul);
+    prune(&mut secret);
+    let div = secret / 32;
+    mix(&mut secret, div);
+    prune(&mut secret);
+    let mul = secret * 2048;
+    mix(&mut secret, mul);
+    prune(&mut secret);
+    secret
+}
+
+fn part_1(input: &str) -> String {
+    let numbers = parse(input);
+    let mut sum = 0;
+    for n in numbers.iter() {
+        let mut secret = *n;
+        for _ in 0..2000u64 {
+            secret = random(secret);
+        }
+        sum += secret;
+    }
+
+    sum.to_string()
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+    let result = part_1(input);
+    println!("Part 1: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input: &str = "1
+10
+100
+2024";
+        assert_eq!(part_1(input), "37327623");
+    }
+}
---
+++ 2024/day-22/src/bin/part-2.rs
@@ -0,0 +1,82 @@
+use std::{cmp::max, collections::HashMap};
+
+fn parse(input: &str) -> Vec<u64> {
+    input.trim()
+        .lines()
+        .map(|l| {
+            l.parse::<u64>().unwrap()
+        }).collect()
+}
+
+fn mix(secret: &mut u64, new_value: u64) {
+    *secret ^= new_value;
+}
+
+fn prune(secret: &mut u64) {
+    *secret %= 16777216;
+}
+
+fn random(mut secret: u64) -> u64 {
+    let mul = secret * 64;
+    mix(&mut secret, mul);
+    prune(&mut secret);
+    let div = secret / 32;
+    mix(&mut secret, div);
+    prune(&mut secret);
+    let mul = secret * 2048;
+    mix(&mut secret, mul);
+    prune(&mut secret);
+    secret
+}
+
+fn part_2(input: &str) -> String {
+    let numbers = parse(input);
+    let mut all_changes: HashMap<(i8, i8, i8,i8), u64> = HashMap::new();
+    for n in numbers.iter() {
+        let mut secret = *n;
+        let mut old_digit = (secret % 10) as i8;
+        let mut diff1 = 0;
+        let mut diff2 = 0;
+        let mut diff3 = 0;
+        let mut changes: HashMap<(i8, i8, i8,i8), u64> = HashMap::new();
+        for i in 0..2000u64 {
+            secret = random(secret);
+            let digit = (secret % 10) as i8;
+            let diff = digit - old_digit;
+            old_digit = digit;
+            if i >= 3 {
+                changes.entry((diff1, diff2, diff3, diff)).or_insert(digit as u64);
+            }
+
+            diff1 = diff2;
+            diff2 = diff3;
+            diff3 = diff;
+        }
+
+        for (k, v1) in changes.iter() {
+            all_changes.entry(*k).and_modify(|v2| *v2 += *v1).or_insert(*v1);
+        }
+    }
+
+    all_changes.values().max().unwrap().to_string()
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+    let result = part_2(input);
+    println!("Part 2: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input: &str = "1
+2
+3
+2024";
+        assert_eq!(part_2(input), "23");
+    }
+}
